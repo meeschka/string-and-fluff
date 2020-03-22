95ded76811fb3d77ebd0cc140354607336065b86
Michelle Pitts <michelle.m.pitts@gmail.com>
2020-03-21 20:09:19 -0400

    updated shop data with unique ids, updated cart reducer to include adding items

--- src/shopData.js
+++ src/shopData.js
@@ -5,25 +5,25 @@ const SHOP_DATA = [
       routeName: 'yarn',
       items: [
         {
-          id: 1,
+          id: 11,
           name: 'Worsted Wool',
           imageUrl: 'https://i.imgur.com/CUl09UV.jpg',
           price: 25
         },
         {
-          id: 2,
+          id: 12,
           name: 'Hand-painted sock yarn',
           imageUrl: 'https://i.imgur.com/7ev7AA8.jpg',
           price: 18
         },
         {
-          id: 3,
+          id: 13,
           name: 'Neutral DK Cotton Collection',
           imageUrl: 'https://i.imgur.com/u9CSK9m.jpg',
           price: 35
         },
         {
-          id: 4,
+          id: 14,
           name: '6-ply aran shetland wool',
           imageUrl: 'https://i.imgur.com/EkHLRBl.jpg',
           price: 25
@@ -36,25 +36,25 @@ const SHOP_DATA = [
       routeName: 'fiber',
       items: [
         {
-          id: 1,
+          id: 21,
           name: 'SW Merino Top',
           imageUrl: 'https://i.imgur.com/3TlIaKx.jpg',
           price: 13
         },
         {
-          id: 2,
+          id: 22,
           name: 'Shetland Top (Light Grey)',
           imageUrl: 'https://i.imgur.com/3TlIaKx.jpg',
           price: 12
         },
         {
-          id: 3,
+          id: 23,
           name: 'Yak/Camel/Merino Blend',
           imageUrl: 'https://i.imgur.com/3TlIaKx.jpg',
           price: 15
         },
         {
-          id: 4,
+          id: 24,
           name: 'Raw Coopworth Fleece',
           imageUrl: 'https://i.imgur.com/vMOAhyJ.jpg',
           price: 75
@@ -67,34 +67,34 @@ const SHOP_DATA = [
       routeName: 'weaving',
       items: [
         {
-          id: 1,
+          id: 31,
           name: '2/20 Wool',
           imageUrl: 'https://i.imgur.com/1n6F60L.jpg',
           price: 26
         },
         {
-          id: 2,
+          id: 32,
           name: '2/18 Mercerized Cotton',
           imageUrl: 'https://i.imgur.com/M1iKf6p.jpg',
           price: 30
         },
         {
-          id: 3,
+          id: 33,
           name: '2/36 Muga Silk',
           imageUrl: 'https://i.imgur.com/4nzScZk.jpg',
           price: 50
         },
         {
-          id: 4,
+          id: 34,
           name: '2/36 Natural Linen',
           imageUrl: 'https://i.imgur.com/bgqIjNs.jpg',
           price: 40
         },
         {
-          id: 5,
+          id: 35,
           name: '2/20 Cottlin',
           imageUrl: 'https://i.imgur.com/FsSq38y.jpg',
-          price: 185
+          price: 70
         }
       ]
     },
@@ -104,31 +104,31 @@ const SHOP_DATA = [
       routeName: 'accessories',
       items: [
         {
-          id: 1,
+          id: 41,
           name: 'Skein Winder',
           imageUrl: 'https://i.imgur.com/ugwvI3q.jpg',
           price: 40
         },
         {
-          id: 2,
+          id: 42,
           name: 'Automatic Skein Winder',
           imageUrl: 'https://i.imgur.com/ugwvI3q.jpg',
           price: 120
         },
         {
-          id: 3,
+          id: 43,
           name: 'Wooden Needles (Straight)',
           imageUrl: 'https://i.imgur.com/FBcaAoq.jpg',
           price: 12
         },
         {
-          id: 4,
+          id: 44,
           name: 'Wooden Needles (Round)',
           imageUrl: 'https://i.imgur.com/f4a21fc.jpg',
           price: 12
         },
         {
-          id: 5,
+          id: 45,
           name: 'Stitch Markers',
           imageUrl: 'https://i.imgur.com/KcuysR8.jpg',
           price: 6
@@ -141,13 +141,13 @@ const SHOP_DATA = [
       routeName: 'sale',
       items: [
         {
-            id: 1,
+            id: 41,
             name: 'Skein Winder',
             imageUrl: 'https://i.imgur.com/ugwvI3q.jpg',
             price: 40
         },
         {
-          id: 5,
+          id: 35,
           name: '2/20 Cottlin',
           imageUrl: 'https://i.imgur.com/FsSq38y.jpg',
           price: 185
