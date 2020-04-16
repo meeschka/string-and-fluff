9e784f931eda25bb4a3f1f4ed89f70e6a4ad681a
Michelle Pitts <michelle.m.pitts@gmail.com>
2020-04-16 10:20:42 -0400

    use cart saga to empty cart on user signout

--- src/redux/shop/shop-saga.js
+++ src/redux/shop/shop-saga.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from 'redux-saga/effects'
+import { takeLatest, all, call, put } from 'redux-saga/effects'
 
 import { ShopActionTypes } from './shop-types'
 import { fetchCollectionsFailure, fetchCollectionsSuccess } from './shop-actions'
@@ -17,4 +17,10 @@ export function* fetchCollectionsAsync() {
 
 export function* fetchCollectionsStart() {
     yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsAsync)
+}
+
+export function* shopSagas() {
+    yield all([
+        call(fetchCollectionsStart)
+    ])
 }
