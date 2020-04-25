775ba3a8c26dc892ce02ffe0b443697ab22524d0
Michelle Pitts <michelle.m.pitts@gmail.com>
2020-04-25 11:01:26 -0400

    added express server to handle backedn stripe payment, separated react to client folder

--- client/src/components/stripe-btn/stripe-btn.jsx
+++ client/src/components/stripe-btn/stripe-btn.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
+import axios from 'axios'
 
 const StripeCheckoutBtn = ({ price }) => {
     const priceForStripe = price * 100
     const publishableKey = 'pk_test_0SsnP4rOxy2nI0G8kz4Ca6gQ00M4lu0XJV'
 
     const onToken = token => {
-        console.log(token)
-        alert('Payment successful')
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(res => {
+            alert('Payment successful')
+        }).catch(err => {
+            console.log('Payment error: ', JSON.parse(err))
+            alert('There was an issue with your payment. Please make sure you use the provided sample credit card')
+        })
+        
     }
 
      return (
